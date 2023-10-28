5d147ea498daf6b8f32d2ca6f3e61e2e4e1487f1
crash1604 <cs.cucse@gmail.com>
2023-10-28 04:55:39 -0400

    login function completed

--- spotify/views.py
+++ spotify/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,7 +14,12 @@ from .serializers import UserSerializer
 
 @api_view(['POST'])
 def login(request):
-    return Response({202})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['POST'])
 def signup(request):
