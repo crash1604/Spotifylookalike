eb49a114ef60ef7e5c79de9bc5c0ecf081a8eed7
crash1604 <cs.cucse@gmail.com>
2024-02-26 10:31:17 -0500

    add album and get album module done

--- album/urls.py
+++ album/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.get_all_albums, name='get_all_albums'),
-    # path('add/',views.add_new_album, name='add_new_album'),
+    path('add/',views.add_new_album, name='add_new_album'),
+    path('tracklist/<str:artist_name>/<str:album_name>/', views.get_tracks_by_album_name_and_artist, name='get_tracks_by_album_name_and_artist'),
     path('<str:artist_name>/<str:album_name>/', views.get_album_by_artist_and_name, name='get_album_by_artist_and_name'),
     path('<str:artist_name>/', views.get_album_by_artist, name='get_album_by_artist')
-    
-    
 ]
--- album/views.py
+++ album/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from datetime import datetime
 
 from .models import Album
+from music.models import Track
+from music.serializers import TrackSerializer
+from .serializers import AlbumSerializer
 from artist.models import Artist
 
 from rest_framework.decorators import api_view, authentication_classes , permission_classes
@@ -20,9 +24,9 @@ def get_all_albums(request):
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def get_album_by_artist_and_name(request,artist_name , album_name):
-    album_files = Album.objects.filter(artist__name__exact=artist_name, title__exact=album_name)
-    data = [{'title': album.title, 'artist': album.artist.name} for album in album_files]
-    return JsonResponse(data, safe=False)
+    album = Album.objects.get(artist__name__exact=artist_name, title__exact=album_name)
+    serializer = AlbumSerializer(album)  
+    return JsonResponse(serializer.data)
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
@@ -34,4 +38,42 @@ def get_album_by_artist(request, artist_name):
     data = [{'title': album.title, 'artist': album.artist.name} for album in album_files]
     return JsonResponse(data, safe=False)
 
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def add_new_album(request):
+    if request.method == "POST":
+        title = request.data.get('title')
+        release_date = datetime.strptime(request.data.get('release_date'),'%Y-%m-%d')
+        artist_id = request.data.get('artist')
+        cover_art = request.data.get('cover_art')
 
+        # Create a new Album object
+        album = Album.objects.create(
+            title=title,
+            release_date=release_date,
+            artist_id=artist_id,
+            cover_art=cover_art
+        )
+
+        # Return a JSON response with the created album data
+        data = {
+            'title': album.title,
+            'release_date': str(album.release_date),
+            'artist': album.artist.name,
+            'cover_art': album.cover_art.url if album.cover_art else None,
+        }
+        return JsonResponse(data, status=201)
+
+    else:
+        return JsonResponse({'error': 'This view requires a POST request'}, status=405)
+    
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_tracks_by_album_name_and_artist(request, artist_name, album_name):
+    album = get_object_or_404(Album, title__iexact=album_name, artist__name__exact=artist_name)
+    trackList = Track.objects.filter(album=album)
+    serializer = TrackSerializer(trackList, many=True)
+    return JsonResponse(serializer.data, safe=False)
